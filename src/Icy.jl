b78e8b9533ea778cb61f6db405c3d4e6ab73a4dd
ferreol <ferreol.soulez@free.fr>
2023-12-11 11:47:08 +0100

    Dispatch on more types

--- src/Icy.jl
+++ src/Icy.jl
@@ -9,30 +9,30 @@ type2number(::Val{Int32})  =2
 type2number(::Val{Float32})=3
 type2number(::Val{Float64})=4
 
-function icy_imshow(a::AbstractArray{T,N}, title="Julia") where {T,N}
-    if N > 4
-        println("Unsupported number of dimension type")
-        return
-    end
-    if N < 2
-        println("Unsupported number of dimension type")
-        return
-    end
+function icy_imshow(a::AbstractArray{T,N}, title="Julia") where {T<:Union{Int8,Int16,Int32,Float32,Float64},N}
+    N > 4 && error("Unsupported number of dimension type")
+    
+    N < 2 && error("Unsupported number of dimension type")
+    
     details = "$(type2number(Val((T))))x$(join(size(a),'x'))#$(title)"
     b = hton.(a)
-
-
+    
+    
     println("Sending Size "*details)
-   @async begin 
-    client = connect(10001)
-    write(client, details)
-    close(client)
-    println("Sending data")
-    client = connect(10001)
-    write(client, b)
-    close(client)
+    @async begin 
+        client = connect(10001)
+        write(client, details)
+        close(client)
+        println("Sending data")
+        client = connect(10001)
+        write(client, b)
+        close(client)
     end
 end
+
+icy_imshow(a::AbstractArray{T,N}, title="Julia") where {T<:Integer,N} = icy_imshow(Int32.(a), title)
+icy_imshow(a::AbstractArray{Bool,N}, title="Julia") where {N} = icy_imshow(Int8.(a), title)
+icy_imshow(a::AbstractArray{T,N}, title="Julia") where {N,T<:Number} = icy_imshow(Float64.(a), title)
 
 
 end # module
